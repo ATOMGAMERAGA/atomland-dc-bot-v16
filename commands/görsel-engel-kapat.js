2dfaff15ef873a636c75f6b61f3ae37b1b4c80b1
Atom Gamer Arda A.G.A <121353232+ATOMGAMERAGA@users.noreply.github.com>
2024-12-18 22:37:00 +0300

    Update görsel-engel-kapat.js

--- commands/görsel-engel-kapat.js
+++ commands/görsel-engel-kapat.js
@@ -2,14 +2,14 @@ const { PermissionsBitField } = require("discord.js");
 const db = require("croxydb")
 module.exports = {
     name:"görsel-engel-kapat",
-    description: '💙 Görsel engel sistemini kapatırsın!',
+    description: ' Görsel engel sistemini kapatırsın!',
     type:1,
     options: [],
   run: async(client, interaction) => {
 
-    if(!interaction.member.permissions.has(PermissionsBitField.Flags.ManageChannels)) return interaction.reply({content: "<:carpi:1040649840394260510> | Kanalları Yönet Yetkin Yok!", ephemeral: true})
+    if(!interaction.member.permissions.has(PermissionsBitField.Flags.ManageChannels)) return interaction.reply({content: "<a:arp:1318968419940569159> | Kanalları Yönet Yetkin Yok!", ephemeral: true})
    db.delete(`görselengel.${interaction.guild.id}`)
-   interaction.reply("<:tik:1039607067729727519> | Görsel Engel başarıyla kapatıldı!")
+   interaction.reply("<a:tik:1318968486671945840> | Görsel Engel başarıyla kapatıldı!")
 }
 
 };
