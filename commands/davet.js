e4d423c36ead456f86f864b09d7267d618e6903a
Atom Gamer Arda A.G.A <121353232+ATOMGAMERAGA@users.noreply.github.com>
2024-12-18 22:29:00 +0300

    Update davet.js

--- commands/davet.js
+++ commands/davet.js
@@ -8,13 +8,13 @@ module.exports = {
 
   run: async(client, interaction) => {
 
-    const dvt = new Discord.ButtonBuilder().setLabel('Davet Linkim').setStyle('Link').setEmoji('1063843026482503739').setURL('https://discord.com/api/oauth2/authorize?client_id=1038372156578480199&permissions=347200&scope=applications.commands%20bot');
-	const destek = new Discord.ButtonBuilder().setLabel('Destek Sunucum').setStyle('Link').setEmoji('1044325557615202364').setURL('https://discord.gg/SY7aMsVpUJ');
+    const dvt = new Discord.ButtonBuilder().setLabel('Davet Linkim').setStyle('Link').setEmoji('1063843026482503739').setURL('https://discord.com/oauth2/authorize?client_id=1285893895804293226&scope=bot&permissions=27649375294');
+	const destek = new Discord.ButtonBuilder().setLabel('Destek Sunucum').setStyle('Link').setEmoji('1044325557615202364').setURL('https://dc.atomland.xyz');
     const row = new Discord.ActionRowBuilder().addComponents(dvt).addComponents(destek)
     const embed = new EmbedBuilder()
-    .setAuthor({ name: "Merhaba, Ben Silex!", iconURL: interaction.client.user.displayAvatarURL({ dynamic: true })})
-.setTitle("Silex'i Davet Et!")
-.setDescription(`<:bot:1039607042291269703> | ATOMLAND'ı şimdi sunucuna davet et ve botun tadını çıkar!`)
+    .setAuthor({ name: "Merhaba, Ben ATOMLAND!", iconURL: interaction.client.user.displayAvatarURL({ dynamic: true })})
+.setTitle("ATOMLAND'ı Davet Et!")
+.setDescription(`<:Discord_Pixel_Emoji:1288881557087453194> | ATOMLAND'ı şimdi sunucuna davet et ve botun tadını çıkar!`)
 .setColor('#2F3136')
 .setTimestamp()
 .setFooter({text: interaction.user.tag+" İstedi.", iconURL: interaction.user.displayAvatarURL({ dynamic: true })})
