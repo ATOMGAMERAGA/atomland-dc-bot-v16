d3356763d0b45e32b8994a025e675e4e264a35ef
Atom Gamer Arda A.G.A <121353232+ATOMGAMERAGA@users.noreply.github.com>
2024-12-19 07:06:53 +0300

    Update kayıt-sistemi-kapat.js

--- commands/kayıt-sistemi-kapat.js
+++ commands/kayıt-sistemi-kapat.js
@@ -3,7 +3,7 @@ const db = require("croxydb")
 const Discord = require("discord.js")
 module.exports = {
     name: "kayıt-sistemi-kapat",
-    description: "💙 Kayıt sistemini kapatırsın!",
+    description: " Kayıt sistemini kapatırsın!",
     type: 1,
     options: [],
     // 
@@ -12,13 +12,13 @@ module.exports = {
         const { user, customId, guild } = interaction;
         const yetki = new Discord.EmbedBuilder()
             .setColor("Red")
-            .setDescription("<:carpi:1040649840394260510> | Bu komutu kullanabilmek için `Yönetici` yetkisine sahip olmalısın!")
+            .setDescription("<a:arp:1318968419940569159> | Bu komutu kullanabilmek için `Yönetici` yetkisine sahip olmalısın!")
 
         if (!interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) return interaction.reply({ embeds: [yetki], ephemeral: true })
 
         const basarili = new EmbedBuilder()
             .setColor("Green")
-            .setDescription(`<:tik:1039607067729727519> | __**Kayıt Sistemi**__ başarıyla sıfırlandı!`)
+            .setDescription(`<a:tik:1318968486671945840> | __**Kayıt Sistemi**__ başarıyla sıfırlandı!`)
             db.delete(`kayıtsistemi_${interaction.guild.id}`)
 			db.delete(`kayıtsistemiDate_${interaction.guild.id}`)
 
