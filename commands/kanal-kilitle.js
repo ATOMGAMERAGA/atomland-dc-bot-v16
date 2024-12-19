bd04640043d552ed009e7751cef724aef0b8e86d
Atom Gamer Arda A.G.A <121353232+ATOMGAMERAGA@users.noreply.github.com>
2024-12-19 07:04:37 +0300

    Update kanal-kilitle.js

--- commands/kanal-kilitle.js
+++ commands/kanal-kilitle.js
@@ -2,16 +2,16 @@ const { Client, EmbedBuilder, PermissionsBitField } = require("discord.js");
 const Discord = require("discord.js")
 module.exports = {
   name: "kanal-kilitle",
-  description: "💙 Kanalı mesaj gönderilmesine kapatır.!",
+  description: " Kanalı mesaj gönderilmesine kapatır.!",
   type: 1,
   options: [],
 
   run: async(client, interaction) => {
-    if(!interaction.member.permissions.has(PermissionsBitField.Flags.ManageChannels)) return interaction.reply({content: "<:carpi:1040649840394260510> | Kanalları yönet yetkin yok!", ephemeral: true})
+    if(!interaction.member.permissions.has(PermissionsBitField.Flags.ManageChannels)) return interaction.reply({content: "<a:arp:1318968419940569159> | Kanalları yönet yetkin yok!", ephemeral: true})
     interaction.channel.permissionOverwrites.edit(interaction.guild.id, {
       SendMessages: false,
     });
-interaction.reply({content: '<:tik:1039607067729727519> | Kanal başarılı bir şekilde mesaj gönderimine kapatıldı!'})
+interaction.reply({content: '<a:tik:1318968486671945840> | Kanal başarılı bir şekilde mesaj gönderimine kapatıldı!'})
   }  
 
 };
