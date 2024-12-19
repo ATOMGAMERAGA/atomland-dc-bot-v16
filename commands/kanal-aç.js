f8a75c42f0759000df63b2e82c939a1ba9d00500
Atom Gamer Arda A.G.A <121353232+ATOMGAMERAGA@users.noreply.github.com>
2024-12-19 07:02:43 +0300

    Update kanal-aç.js

--- commands/kanal-aç.js
+++ commands/kanal-aç.js
@@ -2,16 +2,16 @@ const { Client, EmbedBuilder, PermissionsBitField } = require("discord.js");
 const Discord = require("discord.js")
 module.exports = {
   name: "kanal-aç",
-  description: "💙 Kanalı mesaj gönderimine açar!",
+  description: " Kanalı mesaj gönderimine açar!",
   type: 1,
   options: [],
 
   run: async(client, interaction) => {
-    if(!interaction.member.permissions.has(PermissionsBitField.Flags.ManageChannels)) return interaction.reply({content: "<:carpi:1040649840394260510> | Kanalları yönet yetkin yok!", ephemeral: true})
+    if(!interaction.member.permissions.has(PermissionsBitField.Flags.ManageChannels)) return interaction.reply({content: "<a:arp:1318968419940569159> | Kanalları yönet yetkin yok!", ephemeral: true})
     interaction.channel.permissionOverwrites.edit(interaction.guild.id, {
       SendMessages: true,
     });
-interaction.reply({content: '<:tik:1039607067729727519> | Kanal başarılı bir şekilde mesaj gönderimine açıldı!'})
+interaction.reply({content: '<a:tik:1318968486671945840> | Kanal başarılı bir şekilde mesaj gönderimine açıldı!'})
   }  
 
 };
