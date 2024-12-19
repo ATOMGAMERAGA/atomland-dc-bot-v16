ddfea19248e2ccbddc9226a075957a65da1e0fd0
Atom Gamer Arda A.G.A <121353232+ATOMGAMERAGA@users.noreply.github.com>
2024-12-19 06:43:56 +0300

    Update hg-bb-kapat.js

--- commands/hg-bb-kapat.js
+++ commands/hg-bb-kapat.js
@@ -2,14 +2,14 @@ const { PermissionsBitField } = require("discord.js");
 const db = require("croxydb")
 module.exports = {
     name:"giriş-çıkış-kapat",
-    description: '💙 Giriş Çıkış Sistemini kapatırsın!',
+    description: ' Giriş Çıkış Sistemini kapatırsın!',
     type:1,
     options: [],
   run: async(client, interaction) => {
 
-    if(!interaction.member.permissions.has(PermissionsBitField.Flags.ManageChannels)) return interaction.reply({content: "<:carpi:1040649840394260510> | Kanalları Yönet Yetkin Yok!", ephemeral: true})
+    if(!interaction.member.permissions.has(PermissionsBitField.Flags.ManageChannels)) return interaction.reply({content: "<a:arp:1318968419940569159> | Kanalları Yönet Yetkin Yok!", ephemeral: true})
    db.delete(`hgbb_${interaction.guild.id}`)
-   interaction.reply("<:tik:1039607067729727519> | Hoşgeldin Güle Güle Kanalı Başarıyla Sıfırlandı!")
+   interaction.reply("<a:tik:1318968486671945840> | Hoşgeldin Güle Güle Kanalı Başarıyla Sıfırlandı!")
 }
 
 };
